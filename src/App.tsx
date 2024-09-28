39e6b18d4045ee69fc5c638b330d3598a14d47f9
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-27 18:36:27 -0600

    feat: create App of React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HomePage from './modules/home/presentation/pages/HomePage'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
