25eed7da16b3cef5b1212ae4d4d1914bc65943a5
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-27 21:43:24 -0600

    feat: make responsive the Body

--- src/modules/home/presentation/sections/Body.tsx
+++ src/modules/home/presentation/sections/Body.tsx
@@ -1,30 +1,14 @@
-import { CSSProperties } from 'react';
+import './Body.css';
 import CountDown from '../components/CountDown';
-
 
 export default function Body() {
     return (
-        <div style={styles.body}>
-            <img src="/unicon.png" alt="unicon" style={{ width: '70%' }} />
-            <p style={styles.mission}>Un faro de inspiración y comunidad para los visionarios tecnológicos del mañana.</p>
-            <CountDown/>
+        <div className="body">
+            <div className='content'>
+                <img src="/unicon.png" alt="unicon" className="unicon-image" />
+                <p className="mission">Un faro de inspiración y comunidad para los visionarios tecnológicos del mañana.</p>
+                <CountDown />
+            </div>
         </div>
     );
 }
-
-const styles: {
-    body: CSSProperties,
-    mission: CSSProperties,
-} = {
-    body: {
-        width: '55%',
-        height: '100vh',
-        textAlign: 'center',
-        alignContent: 'center',
-        margin: 'auto',
-    },
-    mission: {
-        fontSize: '1.5rem',
-        marginTop: '20px',
-    },
-};
