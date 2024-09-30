eb2c74232848002b03e6ffac8701645d6ab5cc86
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-30 14:34:39 -0600

    feat: extract compnents to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { CSSProperties } from 'react'
 import './App.css'
+import UAppBar from './modules/app_bar/presentation/UAppBar'
 import CursorAura from './modules/aura/presentation/CursorAura'
+import FAQPage from './modules/faq/presentation/pages/FAQPage'
+import Footer from './modules/footer/presentation/Footer'
 import HomePage from './modules/home/presentation/pages/HomePage'
 
 function App() {
 
   return (
     <>
+      <div style={styles.appBarContainer}>
+        <UAppBar />
+      </div>
       <HomePage />
+      <FAQPage />
+      <Footer />
       <CursorAura />
     </>
   )
 }
+
+const styles: {
+  appBarContainer: CSSProperties,
+} = {
+
+  appBarContainer: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 10,
+  },
+
+};
 
 export default App;
