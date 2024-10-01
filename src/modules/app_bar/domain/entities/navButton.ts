151e6b461711a351f9b21854e3eb5dfbe3ab02b8
reitmas32 <rafa.zamora.rals@gmail.com>
2024-10-01 11:50:52 -0600

    feat:  extract NavButton to entity

---
+++ src/modules/app_bar/domain/entities/navButton.ts
@@ -0,0 +1,8 @@
+export default class NavButton {
+    label: string;
+    path: string;
+    constructor(label: string, path: string) {
+        this.label = label;
+        this.path = path;
+    }
+}
