dfa18a331180871458385140e39209445ae6e049
reitmas32 <rafa.zamora.rals@gmail.com>
2024-10-28 13:26:51 -0600

    feat: add link to form of speakers

--- src/modules/home/presentation/sections/About.tsx
+++ src/modules/home/presentation/sections/About.tsx
@@ -1,4 +1,5 @@
 import { Button, styled } from '@mui/material';
+import 'react-toastify/dist/ReactToastify.css';
 import './About.css';
 
 const UButton = styled(Button)(({ theme }) => ({
@@ -16,25 +17,29 @@ const UButton = styled(Button)(({ theme }) => ({
     },
 }));
 
+const openLinkInNewWindow = (url: string) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+};
 
 export default function About() {
+
     return (
         <>
 
+
             <div className="about-cards-container">
-                
+
                 <div className="about-card">
                     <h3>Quiero ser Speaker</h3>
                     <p>Comparte tu conocimiento en nuestro evento.</p>
-                    <UButton>Me gustaria ser Speaker</UButton>
+                    <UButton onClick={() => openLinkInNewWindow('https://pvoxqrlhbb6.typeform.com/to/iVZe78iV')}>Me gustaria ser Speaker</UButton>
                 </div>
                 <div className="about-card">
                     <h3>Quiero ser Sponsor</h3>
                     <p>Apoya nuestro evento y gana visibilidad.</p>
-                    <UButton>Megustaria ser Sponsor</UButton>
+                    <p>Envianos un correo a soporte@unicon.mx</p>
                 </div>
             </div>
-            
         </>
 
     );
