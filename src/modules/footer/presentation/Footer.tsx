546ac104b6fa57ae0edb5d6130db26e59d093198
reitmas32 <rafa.zamora.rals@gmail.com>
2024-10-28 10:47:40 -0600

    feat: remove links of Footter

--- src/modules/footer/presentation/Footer.tsx
+++ src/modules/footer/presentation/Footer.tsx
@@ -1,4 +1,3 @@
-import DiscordButton from '../../app_bar/presentation/atoms/DiscordButton';
 import GitHubButton from '../../app_bar/presentation/atoms/GitHubButton';
 import InstagramButton from '../../app_bar/presentation/atoms/InstagramButton';
 import './Footer.css';
@@ -11,14 +10,17 @@ export default function Footer() {
         <ul className="footer-links" style={{ display: 'flex', justifyContent: 'center', gap: '30px', marginTop: '20px', marginBottom: '20px', }}>
           <GitHubButton />
           <InstagramButton />
-          <DiscordButton />
         </ul>
-        <ul className="footer-links">
-          <li><a href="/about">Sobre nosotros</a></li>
-          <li><a href="/contact">Contacto</a></li>
-          <li><a href="/privacy">Política de privacidad</a></li>
-          <li><a href="/terms">Términos de servicio</a></li>
-        </ul>
+        {
+          /*
+          <ul className="footer-links">
+            <li><a href="/about">Sobre nosotros</a></li>
+            <li><a href="/contact">Contacto</a></li>
+            <li><a href="/privacy">Política de privacidad</a></li>
+            <li><a href="/terms">Términos de servicio</a></li>
+          </ul>
+          */
+        }
       </div>
     </footer>
   );
