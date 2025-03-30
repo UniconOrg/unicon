a4ea9b63487d03fea6ef860867a291bb615f17ef
reitmas32 <rafa.zamora.rals@gmail.com>
2025-03-30 03:52:27 -0600

    feat: add RootLayout and Home components with global styles and background elements

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import CountDown from "./home/components/CountDown";
+export default function Home() {
+  return (
+      <main className="">
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+          <Image
+            src="/unicon.png"
+            alt="Logo"
+            width={800}
+            height={800}
+            className="w-1/2 object-contain"
+            quality={100}  // Opcional: para mayor calidad
+          />
+          <CountDown />
+
+        </div>
+      </main>
+  );
+}
